d24cd68ed844cc572c449547f31378d71cdc6328
Kaja Królikowska <krolikowska.kaja@gmail.com>
2022-09-14 12:52:28 +0200

    Added function displayCurrentConditions
    displaying current temperature, conditions, wind and humidity in the selected city

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function displayCurrentConditions(response) {
+  console.log(response.data);
+  let currentTemperature = document.querySelector("#temperature");
+  currentTemperature.innerHTML = Math.round(response.data.main.temp);
+  let currentCity = document.querySelector("#city");
+  currentCity.innerHTML = response.data.name;
+  let currentConditions = document.querySelector("#description");
+  currentConditions.innerHTML = response.data.weather[0].description;
+  let currentWind = document.querySelector("#wind");
+  currentWind.innerHTML = response.data.wind.speed;
+  let currentHumidity = document.querySelector("#humidity");
+  currentHumidity.innerHTML = response.data.main.humidity;
+}
+
+let apiKey = "8ade40df9cf169461fc7f8acab2e9ac0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayCurrentConditions);

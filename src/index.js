8eac8f7a47866ce969b2b42920b51b18f03ade89
Kaja Królikowska <krolikowska.kaja@gmail.com>
2022-09-15 11:59:34 +0200

    Cleanup js file, adding Date function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+}
 function displayCurrentConditions(response) {
   console.log(response.data);
   let currentTemperature = document.querySelector("#temperature");
-  currentTemperature.innerHTML = Math.round(response.data.main.temp);
   let currentCity = document.querySelector("#city");
-  currentCity.innerHTML = response.data.name;
   let currentConditions = document.querySelector("#description");
-  currentConditions.innerHTML = response.data.weather[0].description;
   let currentWind = document.querySelector("#wind");
-  currentWind.innerHTML = Math.round(response.data.wind.speed);
   let currentHumidity = document.querySelector("#humidity");
+  let locationDate = document.querySelector("#localDate");
+  let locationTime = document.querySelector("#localTime");
+
+  currentTemperature.innerHTML = Math.round(response.data.main.temp);
+  currentCity.innerHTML = response.data.name;
+  currentConditions.innerHTML = response.data.weather[0].description;
+  currentWind.innerHTML = Math.round(response.data.wind.speed);
   currentHumidity.innerHTML = response.data.main.humidity;
+  locationDate = formatDate(reponse.data.dt * 1000);
 }
 
 let apiKey = "8ade40df9cf169461fc7f8acab2e9ac0";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=${apiKey}&units=metric`;
+let city = "Warszawa";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayCurrentConditions);
-
-function time() {
-  console.log(Date);
-}

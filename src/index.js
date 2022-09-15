91ae9bcf5fea91254ca159a5c5856ae2234be4f5
Kaja Królikowska <krolikowska.kaja@gmail.com>
2022-09-15 12:36:04 +0200

    Set up formatDate function, display current date and time.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
+  let hours = String(date.getHours()).padStart(2, "0");
+  let minutes = String(date.getMinutes()).padStart(2, "0");
+
+  let num = date.getDate();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let year = date.getFullYear();
+  return `${day}, ${num} ${month} ${year} <br /> ${hours}:${minutes}`;
 }
+
 function displayCurrentConditions(response) {
   console.log(response.data);
   let currentTemperature = document.querySelector("#temperature");
@@ -10,19 +40,18 @@ function displayCurrentConditions(response) {
   let currentConditions = document.querySelector("#description");
   let currentWind = document.querySelector("#wind");
   let currentHumidity = document.querySelector("#humidity");
-  let locationDate = document.querySelector("#localDate");
-  let locationTime = document.querySelector("#localTime");
+  let locationDate = document.querySelector("#local-date");
 
   currentTemperature.innerHTML = Math.round(response.data.main.temp);
   currentCity.innerHTML = response.data.name;
   currentConditions.innerHTML = response.data.weather[0].description;
   currentWind.innerHTML = Math.round(response.data.wind.speed);
   currentHumidity.innerHTML = response.data.main.humidity;
-  locationDate = formatDate(reponse.data.dt * 1000);
+  locationDate.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "8ade40df9cf169461fc7f8acab2e9ac0";
-let city = "Warszawa";
+let city = "Sydney";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);

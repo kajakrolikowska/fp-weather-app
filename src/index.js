24f7daf420ea4a399923bbec6482d3c5cd7452c4
Kaja Królikowska <krolikowska.kaja@gmail.com>
2022-09-18 09:49:32 +0200

    Added current location and city search buttons + functions.

--- src/index.js
+++ src/index.js
@@ -55,9 +55,38 @@ function displayCurrentConditions(response) {
   );
 }
 
-let apiKey = "8ade40df9cf169461fc7f8acab2e9ac0";
-let city = "Warsaw";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+//(1) current location:
+function currentPosition(position) {
+  let apiKey = "8ade40df9cf169461fc7f8acab2e9ac0";
+  let lon = position.coords.longitude;
+  let lat = position.coords.latitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
-console.log(apiUrl);
-axios.get(apiUrl).then(displayCurrentConditions);
+  axios.get(apiUrl).then(displayCurrentConditions);
+}
+
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(currentPosition);
+}
+
+let currentButton = document.querySelector(".location");
+currentButton.addEventListener("click", getCurrentPosition);
+
+//(2) search for a specific city-location:
+function getName(city) {
+  let apiKey = "8ade40df9cf169461fc7f8acab2e9ac0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayCurrentConditions);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  getName(city);
+}
+
+let searchForm = document.querySelector("#city-search");
+searchForm.addEventListener("submit", handleSubmit);
+
+getName("Berlin");
